ab25c65d4735e1fb11c6935e9e8b40e1816c57f0
owenpark <owenmmp@gmail.com>
2024-06-01 15:31:47 -0400

    Change background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full scroll-smooth">
       <body
-        className={cn(
-          "relative h-full font-sans antialiased bg-gray-100",
-          inter.className
-        )}
+        className={cn("relative h-full font-sans antialiased", inter.className)}
       >
         <main className="relative flex flex-col min-h-screen">
           <div className="flex-grow flex-1">{children}</div>
